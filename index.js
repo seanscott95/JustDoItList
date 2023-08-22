dc6b0acc4cfce0056f10fb31ac5a057c47d951f2
Sean <seanms418@gmail.com>
2023-08-22 17:06:16 +1000

    halfway through the editItem functionality, current functionality includes, changing the add item button to updated item, adding the clicked on item to the user input, changing the opacity of the clicked on item while in edit mode, if an item is clicked on the style of the element will change as it is in edit mode, but if a new list item is clicked the previous element will have its normal styling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const itemInputEl = document.getElementById('itemInput');
 const itemListEl = document.getElementById('itemList');
 const btnClearEl = document.getElementById('clear');
 const filterInputEl = document.getElementById('filter');
+let isEditMode = false;
 
 // Adds the item inputted to the list
 const addItem = (e) => {
@@ -10,7 +11,7 @@ const addItem = (e) => {
 
     const newItem = itemInputEl.value.trim();
     if (newItem === '') return;
-  
+
     const li = document.createElement('li');
 
     const btn = document.createElement('button');
@@ -26,9 +27,46 @@ const addItem = (e) => {
     itemInputEl.value = '';
 };
 
+const onItemClick = (e) => {
+    if (e.target.closest('li')) {
+        editItem(e.target);
+    };
+    if (e.target.parentElement.classList.contains('removeItem')) {
+        deleteItem(e);
+    };
+};
+
+const editItem = (item) => {
+    isEditMode = true;
+
+    // Removes the class editMode for all list items, if an element was already being edited
+    // and a new element was clicked the class will still be on the previous item as well, 
+    // this will remove the class of the previously edited item
+    itemListEl
+        .querySelectorAll('li')
+        .forEach((i) => i.classList.remove('editMode'));
+
+    item.classList.add('editMode');
+
+    const updateButton = document.createElement('button');
+    updateButton.className = 'btn';
+    updateButton.style.backgroundColor = '#228B22';
+
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-pen';
+
+    updateButton.append(icon, ' Update Item');
+
+    // Replaces the Add Item button with an Update Item Button
+    const formBtn = formEl.querySelector('button');
+    formBtn.replaceWith(updateButton);
+
+    itemInputEl.value = item.innerText;
+}
+
 // Clears all items from the list
 const clearAllItems = () => {
-    while(itemListEl.firstChild) itemListEl.removeChild(itemListEl.firstChild);
+    while (itemListEl.firstChild) itemListEl.removeChild(itemListEl.firstChild);
 };
 
 // Deletes the item when the red x button is clicked
@@ -58,7 +96,7 @@ const filterItem = (e) => {
 const init = () => {
     formEl.addEventListener('submit', addItem);
     btnClearEl.addEventListener('click', clearAllItems);
-    itemListEl.addEventListener('click', deleteItem);
+    itemListEl.addEventListener('click', onItemClick);
     filterInputEl.addEventListener('input', filterItem);
 };
 
