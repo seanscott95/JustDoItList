ab67f2b5dec7eb742fbb2d4e878a0f38625dab8b
Sean <seanms418@gmail.com>
2023-08-22 16:23:45 +1000

    created the filter funtionality, when the user enter input into the filter items input above the list, the list item will appear if it contains that user input and will not display if it doesn't, when the filter input is empty all list items are all still displayed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const formEl = document.getElementById('itemForm');
 const itemInputEl = document.getElementById('itemInput');
 const itemListEl = document.getElementById('itemList');
 const btnClearEl = document.getElementById('clear');
+const filterInputEl = document.getElementById('filter');
 
 // Adds the item inputted to the list
 const addItem = (e) => {
@@ -37,10 +38,28 @@ const deleteItem = (e) => {
     };
 };
 
+// Filters items in the list by the filter items input
+const filterItem = (e) => {
+    const listItems = itemListEl.querySelectorAll('li');
+    const userInput = e.target.value.toLowerCase().trim();
+
+    listItems.forEach((item) => {
+        const itemText = item.innerText.toLowerCase();
+        // If the user input is in the list items name then that listItem will
+        // display in the list, if it doesn't the item will not be displayed
+        if (itemText.indexOf(userInput) != -1) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        };
+    });
+};
+
 const init = () => {
     formEl.addEventListener('submit', addItem);
     btnClearEl.addEventListener('click', clearAllItems);
     itemListEl.addEventListener('click', deleteItem);
+    filterInputEl.addEventListener('input', filterItem);
 };
 
 init();
